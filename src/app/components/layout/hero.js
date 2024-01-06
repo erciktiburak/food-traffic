f9b44f3d2567b3f94f58509b081337c80ffa00aa
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-01-06 12:11:39 +0200

    hero section edited

---
+++ src/app/components/layout/hero.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Hero() {
+    return (
+            <section>
+                <h1>Where Flavor Meets Freshness: Pizza Passion!</h1>
+                    <p>
+                    We invite all pizza enthusiasts to this delightful feast filled with exquisite aromas because when it comes to pizza, we are the ultimate destination!
+                    </p>      
+                    <Image src="" alt={'pizza'}/>
+            </section>
+    );
+}
